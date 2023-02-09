d75d95f3275aa679a5d30e6ac4999fef1d3d0758
griffn <37548204+killgriff22@users.noreply.github.com>
2023-02-09 11:06:48 -0800

    Update miracle.sh

--- miracle.sh
+++ miracle.sh
@@ -1,7 +1,6 @@
 #NOTE THIS IS MEANT FOR WINDOWS
 #DO NOT ATTEMPT TO RUN ON MAC OR LINUX
 powershell ./miracle.ps1
-cd ..
-touch worker
-echo "rm -rf ./lodsamone/" >> worker
-sh worker
+cp worker.sh ../
+cd ../
+sh ./worker.sh
